4a7da4945d4265d4727d9e67a2598085cf10a70c
君赏 <josercc@users.noreply.github.com>
2019-12-06 10:39:34 +0800

    Update public-ggpaysdk-command.rb

--- public-ggpaysdk-command.rb
+++ public-ggpaysdk-command.rb
@@ -3,13 +3,18 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class PublicGgpaysdkCommand < Formula
   desc "发布原生支付脚本"
-  homepage ""
-  url "https://github.com/josercc/Public-GGPaySDK-Command/archive/0.1.3.tar.gz"
-  sha256 "6bfdce74bbae64d73bef4310ae8cd8b48a9d833917bcede5d9054a9e5247d53b"
+  homepage "https://github.com/josercc/Public-GGPaySDK-Command"
+  head "https://github.com/josercc/Public-GGPaySDK-Commandgit"
 
-  #depends_on "xcode" => ["11.0", :build]
-
+#   depends_on :xcode => "11"
+  stable do
+    version "0.1.3"
+    url "https://github.com/josercc/Public-GGPaySDK-Command/archive/0.1.3.tar.gz"
+    sha256 "6bfdce74bbae64d73bef4310ae8cd8b48a9d833917bcede5d9054a9e5247d53b"
+  end
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+    system "swift", "build", "--disable-sandbox"
+    system "mv", ".build/x86_64-apple-macosx/debug/Public-GGPaySDK", "pgg"
+    bin.install "pgg"
   end
 end
