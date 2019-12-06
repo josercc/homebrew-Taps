454fc2bc61d4371cc7adda30b1c7bcd652d6dd38
君赏 <josercc@users.noreply.github.com>
2019-12-06 11:47:05 +0800

    Update public-ggpaysdk-command.rb

--- public-ggpaysdk-command.rb
+++ public-ggpaysdk-command.rb
@@ -8,9 +8,9 @@ class PublicGgpaysdkCommand < Formula
 
 #   depends_on :xcode => "11"
   stable do
-    version "0.1.4"
-    url "https://github.com/josercc/Public-GGPaySDK-Command/archive/0.1.4.tar.gz"
-    sha256 "cd109cb8ab5a7a0c9b8bd8597d3cf0434924d8ecc96c749e893e16212d941f87"
+    version "0.1.5"
+    url "https://github.com/josercc/Public-GGPaySDK-Command/archive/0.1.5.tar.gz"
+    sha256 "a3ca5aa433b045bfad7005364e6008e76e84b5649172b7e0d9758ce9aab7d63e"
   end
   def install
     system "swift", "build", "--disable-sandbox"
