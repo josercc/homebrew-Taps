b52deb79261663e74cc45c2c593d6b59773ac8c6
君赏 <josercc@users.noreply.github.com>
2019-12-06 09:41:52 +0800

    Update public-ggpaysdk-command.rb

--- public-ggpaysdk-command.rb
+++ public-ggpaysdk-command.rb
@@ -7,7 +7,7 @@ class PublicGgpaysdkCommand < Formula
   url "https://github.com/josercc/Public-GGPaySDK-Command/archive/0.1.0.tar.gz"
   sha256 "ea3b8de2b574bfcff98d58f52b0e929c802cd272057c44f87a4e41e397cee675"
 
-  depends_on "xcode" => ["11.0", :build]
+  #depends_on "xcode" => ["11.0", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
