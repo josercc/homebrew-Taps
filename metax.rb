7fab613e1abd5f9a1a5e2f6b0524bf1384d70133
josercc <josercc@163.com>
2025-12-25 13:43:21 +0800

    发布棉宇宙工具0.0.77

--- metax.rb
+++ metax.rb
@@ -2,13 +2,13 @@
 class Metax < Formula
     desc "棉宇宙app开发工具"
     homepage "https://github.com/WinnerApp/metax"
-    url "https://appwrite.winnermedical.com/v1/storage/buckets/67dc1b1300345bcb40a3/files/693958eaab37b3851a0b/download?project=677f626b0012252b422e&mode=admin"
-    sha256 "4550888e24aa64e39687d542c8a49fb0e3f47f2270dac86f4b3b19d98bb5b58c"
+    url "https://appwrite.winnermedical.com/v1/storage/buckets/67dc1b1300345bcb40a3/files/694ccef72d6586dfdac7/download?project=677f626b0012252b422e&mode=admin"
+    sha256 "85186d23e1e69e3ff830a3ce2ee315459dccc99cddcbb24499b129af1dd06ac1"
     license "MIT"
-    version "0.0.76"
+    version "0.0.77"
   
     def install
-      File.rename("0.0.76-4550888e24aa64e39687d542c8a49fb0e3f47f2270dac86f4b3b19d98bb5b58c", "metax")
+      File.rename("0.0.77-85186d23e1e69e3ff830a3ce2ee315459dccc99cddcbb24499b129af1dd06ac1", "metax")
       bin.install "metax"
     end
   
